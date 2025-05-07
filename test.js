56a7e6419caa31a7ff0f04511a9f66ac5d5504a6
Julien Jespersen <julien.jespersen@unige.ch>
2025-05-07 12:31:14 +0200

    feat: more fields, reset button

--- test.js
+++ test.js
@@ -1,10 +1,28 @@
 const app = {
     data() {
         return {
-            test3: 'test3',
+            // personal informations 
             firstName: (firstName = localStorage.getItem('firstName')) ? firstName : '',
             lastName: (lastName = localStorage.getItem('lastName')) ? lastName : '',
+            maidenName: (maidenName = localStorage.getItem('maidenName')) ? maidenName : '',
+            birthDate: (birthDate = localStorage.getItem('birthDate')) ? birthDate : '',
+            gender: (gender = localStorage.getItem('gender')) ? gender : '',
+            nativeLanguage: (nativeLanguage = localStorage.getItem('nativeLanguage')) ? nativeLanguage : '',
+            languages: (languages = localStorage.getItem('languages')) ? languages : '',
+            maritalStatus: (maritalStatus = localStorage.getItem('maritalStatus')) ? maritalStatus : '',
+            // citizenship informations
+            nationality: (nationality = localStorage.getItem('nationality')) ? nationality : '',
+            originCanton: (originCanton = localStorage.getItem('originCanton')) ? originCanton : '',
+            authorisation: (authorisation = localStorage.getItem('authorisation')) ? authorisation : '',
+            // contact informations
             email: (email = localStorage.getItem('email')) ? email : '',
+            phone: (phone = localStorage.getItem('phone')) ? phone : '',
+            residency: (residency = localStorage.getItem('residency')) ? residency : '',
+            address: (address = localStorage.getItem('address')) ? address : '',
+            postalCode: (postalCode = localStorage.getItem('postalCode')) ? postalCode : '',
+            city: (city = localStorage.getItem('city')) ? city : '',
+            country: (country = localStorage.getItem('country')) ? country : '',
+
             blankLine: '',
             docDefinition: { 
                 // Document Meta Data (Visible in document properties)
@@ -37,7 +55,9 @@ const app = {
                     normal: true,
                     bold: false,
                     italics: false,
-                    bolditalics: false
+                    bolditalics: false,
+                    marginBottom: 10,
+
                 },
                 styles: {
                     "text-left": {
@@ -54,8 +74,24 @@ const app = {
                       bold: true,
                       color: '#555',
                     },
+                    'value': {
+                      fontSize: 12,
+                      color: '#000',
+                      background: '#eff',
+                      marginBottom: 10,
+
+                    },
                     'bold': {
                       bold: true
+                    },
+                    'underline': {
+                      color: '#777',
+                      background: '#eff',
+
+                    //   lineHeight: 2,
+                        fontSize: 18,
+                    //   decoration: 'underline',
+                      marginBottom: 10,
                     },
                 },
           
@@ -66,11 +102,37 @@ const app = {
         }
     },
     watch: {
+        // personal informations 
         firstName: function () {localStorage.setItem('firstName', this.firstName)},
         lastName: function () {localStorage.setItem('lastName', this.lastName)},
+        maidenName: function () {localStorage.setItem('maidenName', this.maidenName)},
+        birthDate: function () {localStorage.setItem('birthDate', this.birthDate)},
+        gender: function () {localStorage.setItem('gender', this.gender)},
+        nativeLanguage: function () {localStorage.setItem('nativeLanguage', this.nativeLanguage)},
+        languages: function () {localStorage.setItem('languages', this.languages)},
+        maritalStatus: function () {localStorage.setItem('maritalStatus', this.maritalStatus)},
+        // citizenship informations
+        nationality: function () {localStorage.setItem('nationality', this.nationality)},
+        originCanton: function () {localStorage.setItem('originCanton', this.originCanton)},
+        authorisation: function () {localStorage.setItem('authorisation', this.authorisation)},
+        // contact informations
         email: function () {localStorage.setItem('email', this.email)},
+        phone: function () {localStorage.setItem('phone', this.phone)},
+        residency: function () {localStorage.setItem('residency', this.residency)},
+        address: function () {localStorage.setItem('address', this.address)},
+        postalCode: function () {localStorage.setItem('postalCode', this.postalCode)},
+        city: function () {localStorage.setItem('city', this.city)},
+        country: function () {localStorage.setItem('country', this.country)},
     },    
     methods: {
+        reset(event) {
+            document.querySelector('form').addEventListener("reset", (event) => { 
+                confirm("Are you sure you want to reset the form?") ? localStorage.clear() : event.preventDefault();
+                // localStorage.clear();
+            })
+        },
+
+
         createBlankLine: function (chars) {
             let blankLine = ''
             for (let i = 0; i < chars; i++) {
@@ -80,7 +142,7 @@ const app = {
         },
         populateDocDefinition() {
             this.docDefinition.content = []
-            document.querySelectorAll("#app form input[type='text']").forEach((input) => {
+            document.querySelectorAll('#app form input, #app form select').forEach((input) => {
                 console.log(input.value)
                 this.docDefinition.content.push(
                     {
@@ -88,14 +150,24 @@ const app = {
                         style: 'entry'
                     }
                 )
+            if (input.value !== '') {
                 this.docDefinition.content.push(
                     {
-                        text: (input.value ? input.value : this.blankLine),
+                        text: input.value,
+                        style: 'value',
                         marginBottom: 10
                     }
                 )
+            } else {
+                    this.docDefinition.content.push(
+                        {
+                            text: this.blankLine,
+                            style: 'underline',
+                            // marginBottom: 10
+                        }
+                    )
+                }
             })
-            
         },
         printOut() {
             this.populateDocDefinition()
@@ -124,7 +196,7 @@ const app = {
         
     },
     mounted() {
-        this.blankLine = this.createBlankLine(30)
+        this.blankLine = this.createBlankLine(20)
     }
 }
 
